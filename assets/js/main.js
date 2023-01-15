1532e3da59447f7fa26cd68fc40e85044538f308
unknown <hubert780225@gmail.com>
2023-01-16 02:30:39 +0800

    fix/新增 手機版選單以及隱私權、修改樣式

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,70 +1,31 @@
-/*
-    Handle Parallax Effect
-*/
-const handleJarallaxFunc = () => {
-    const parallax = document.querySelectorAll('.jarallax');
-    jarallax(parallax);
-}
-
-
-/*
-    Handle Waypoint Effect
-*/
-const handleWaypointFunc = () => {
-    // const target = Array.from(document.getElementsByClassName('scroll__animation'));
-    const target = [...document.getElementsByClassName('scroll__animation')];
-
-    const onScrollInit = ( items ) => {
-        items.forEach( item => {
-                const osAnimationClass = item.getAttribute('data-os-animation'),
-                osAnimationDelay = item.getAttribute('data-os-animation-delay'),
-                osAnimationFrequency = item.getAttribute('data-os-animation-frequency');
-
-            item.style.animationDelay = osAnimationDelay;
-
-            const waypoint = new Waypoint({
-                element: item,
-                handler: function(direction) {
-                    if (osAnimationFrequency !== 'once') {
-                        item.classList.add('animated');
-                        item.classList.toggle(osAnimationClass);
-                    }
-
-                    item.classList.add('animated');
-                    item.classList.add(osAnimationClass);
-                },
-                offset: '90%'
-            })
-        });
-    }
-
-    onScrollInit(target);
-}
-
-
-/*
-    Handle Menu Toggle Effect
+/**
+ *  Handle Menu Toggle Effect
  */
 const handleMenuToggle = () => {
-    const [btn, header, menu, modal, modalInner] = ['menu__wrap', 'header', 'cover__menu', 'modal', 'modal__inner']
-        .map(item => document.getElementById(item));
+    const $menuTrigger = $('#menu__wrap');
+    const $modalWrap = $('#modal');
+    const $modal = $('#modal__inner');
 
-    const items = [header, menu, modal, modalInner];
-
-    btn.addEventListener('click', function() {
-        items.map(item => {
-            const isOpen = item.classList.contains("open");
-
-            item.classList.remove('open');
-            item.classList.add('close');
-
-            if (!isOpen) {
-                item.classList.add('open');
-                item.classList.remove('close');
-            }
-        })
-    }, false);
+    $menuTrigger.on('click', function() {
+        $modalWrap.toggleClass('open');
+        $modal.toggleClass('open');
+    });
 }
+
+/**
+ *  Handle Privacy Toggle Effect
+ */
+const handlePrivacyToggle = () => {
+    const $menuTrigger = $('#menu__wrap');
+    const $modalWrap = $('#modal');
+    const $modal = $('#modal__inner');
+
+    $menuTrigger.on('click', function() {
+        $modalWrap.toggleClass('open');
+        $modal.toggleClass('open');
+    });
+}
+
 
 
 /*
