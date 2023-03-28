9d62de0b291cb315689978b7b9beb6a352af3d07
Hubert Hsieh <hubert780225@gmail.com>
2023-03-28 17:03:25 +0800

    fix/修改 20230328 - 網頁需求調整

--- assets/js/main.js
+++ assets/js/main.js
@@ -217,6 +217,58 @@ const handleArticlesControll = () => {
     });
 }
 
+const handleYoutubeControl = () => {
+    // Get element
+    var youtubeEmbedElement = document.getElementById("youtubeEmbed");
+
+    // Add YouTube API script
+    const tag = document.createElement("script");
+    tag.src = "https://www.youtube.com/iframe_api";
+    const firstScriptTag = document.getElementsByTagName("script")[0];
+    firstScriptTag.parentNode.insertBefore(tag, firstScriptTag);
+
+    let player;
+    const videoId = youtubeEmbedElement && youtubeEmbedElement.dataset.videoId;
+    var startSeconds = 0;
+    var endSeconds = 210;
+
+    onYouTubeIframeAPIReady = function () {
+        player = new YT.Player("youtubeEmbed", {
+            videoId: videoId, // YouTube Video ID
+            playerVars: {
+                autoplay: 1, // Auto-play the video on load
+                autohide: 1, // Hide video controls when playing
+                disablekb: 1,
+                controls: 0, // Hide pause/play buttons in player
+                showinfo: 0, // Hide the video title
+                modestbranding: 1, // Hide the Youtube Logo
+                loop: 1, // Run the video in a loop
+                fs: 0, // Hide the full screen button
+                rel: 'preload',
+                enablejsapi: 1,
+                start: startSeconds,
+                end: endSeconds
+            },
+            events: {
+                onReady: function (e) {
+                    e.target.mute();
+                    e.target.playVideo();
+                },
+                onStateChange: function (e) {
+                    if (e.data === YT.PlayerState.PLAYING) {
+                        document.getElementById("youtubeEmbed").classList.add("loaded");
+                    }
+
+                    if (e.data === YT.PlayerState.ENDED) {
+                        // Loop from starting point
+                        player.seekTo(startSeconds);
+                    }
+                }
+            }
+        });
+    };
+}
+
 document.addEventListener("DOMContentLoaded", function(event) {
     // handleJarallaxFunc();
     handleMenuToggle();
@@ -225,4 +277,5 @@ document.addEventListener("DOMContentLoaded", function(event) {
     handleDataAnimation();
     handleSwiperControll();
     handleArticlesControll();
+    // handleYoutubeControl();
 });
